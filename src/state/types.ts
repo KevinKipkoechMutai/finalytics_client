1a9f0d7c8ec91706772fe1e9397d84f69ddbfe9d
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-07-05 15:33:58 +0300

    feat: row1 plots

---
+++ src/state/types.ts
@@ -0,0 +1,24 @@
+export interface ExpensesByCategory {
+    salaries: number;
+    supplies: number;
+    services: number;
+}
+
+export interface Month {
+    id: string;
+    month: string;
+    revenue: number;
+    expenses: number;
+    nonOperationalExpenses: number;
+    operationalExpenses: number;
+}
+
+export interface GetKpisResponse {
+    id: string;
+    _id: string;
+    totalProfit: number;
+    totalRevenue: number;
+    totalExpenses: number;
+    expensesByCategory: ExpensesByCategory;
+    monthlyData: Array<Month>;
+}
