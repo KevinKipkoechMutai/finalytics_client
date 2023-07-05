26177bb4de650dccb5f4f3e84e943cadcb8445c8
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-07-05 20:48:31 +0300

    feat: transactions API schema

--- src/state/types.ts
+++ src/state/types.ts
@@ -36,3 +36,14 @@ export interface GetProductsResponse {
     createdAt: string;
     updatedAt: string;
 }
+
+export interface GetTransactionsResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    buyer: string,
+    amount: number;
+    productIds: Array<string>;
+    createdAt: string;
+    updatedAt: string;
+}
